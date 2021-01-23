c46e4a28b2ba75806bf9221343953504ddd0da98
Ayushvats9068 <77826246+Ayushvats9068@users.noreply.github.com>
2021-01-23 13:39:07 +0530

    Add files via upload

---
+++ day 3 assignment/index.js
@@ -0,0 +1,11 @@
+// What is the value of clothes[0] and why?
+// const clothes = ['jacket', 't-shirt'];
+// clothes.length = 0;
+// console.log(clothes[0]);
+
+
+const clothes = ['jacket', 't-shirt'];
+clothes.length = 0;
+console.log(clothes[0]);
+
+// this programme given an undefined output.because length of clothes are given but not on array.
---
+++ day 3 assignment/index2.js
@@ -0,0 +1,15 @@
+// Write a Javascript program to find the sum of all elements of a given array?
+
+
+var arr = [1, 2, 3, 4, 5],
+    sum = 0,
+    i;
+
+
+for (i = 0; i < arr.length; i++) {
+
+
+    sum = sum + arr[i];
+
+}
+console.log(sum);
